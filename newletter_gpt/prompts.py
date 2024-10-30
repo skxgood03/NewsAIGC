751ee747626c1e662caa2ef81a8fb0590618cdc7
maple@max <feng.liang@kaust.edu.sa>
2024-10-30 18:49:33 +0800

    update to openai > 1.0

--- newletter_gpt/prompts.py
+++ newletter_gpt/prompts.py
@@ -14,11 +14,12 @@ def gen_summary_and_tags_via_llm(feed_item: FeedItem,
                                  api_base: str,
                                  api_key: str,
                                  chatgpt_deployment_name: str):
-    # set up openai
-    openai.api_key = api_key
-    openai.api_version = "2024-09-01-preview"
-    openai.api_type = "azure"
-    openai.api_base = api_base
+    client = openai.AzureOpenAI(
+        azure_endpoint=api_base,
+        api_version="2024-09-01-preview",
+        api_key=api_key,
+        azure_deployment=chatgpt_deployment_name
+    )
     # truncate content, max ~5000 Chinese and English character
     item_content = feed_item.content[:7000]
     function_name = "article_record"
@@ -49,6 +50,55 @@ consumer_electronics: 消费电子，智能手机，智能手表等内容
 请调用 `{function_name}` 这个函数来完成我的要求。
 """
     messages = [{"role": "user", "content": user_message}]
+    tools = [
+        {
+            "type": "function",
+            "function": {
+                "name": function_name,
+                "description": "记录文章的总结和标签",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "summary": {
+                            "type": "string",
+                            "description": "中文的简短的不超过300字的文章总结"
+                        },
+                        "aigc": {
+                            "type": "boolean",
+                            "description": "文章是否含有生成式AI，大语言模型，文生图模型等的相关内容"
+                        },
+                        "digital_human": {
+                            "type": "boolean",
+                            "description": "文章是否含有数字人，动作捕捉，面部捕捉，数字人形艺术等的相关内容"
+                        },
+                        "neural_rendering": {
+                            "type": "boolean",
+                            "description": "文章是否含有神经渲染器，NeRF，可微分渲染等的相关内容"
+                        },
+                        "computer_graphics": {
+                            "type": "boolean",
+                            "description": "文章是否含有图形学，渲染器，渲染，几何处理，图像处理等的相关内容"
+                        },
+                        "computer_vision": {
+                            "type": "boolean",
+                            "description": "文章是否含有计算机视觉，图像分类，目标检测，图像分割，语义分割，深度估计等的相关内容。注意: 脑机接口相关内容不属于计算机视觉的范畴"
+                        },
+                        "robotics": {
+                            "type": "boolean",
+                            "description": "文章是否含有机器人，机器狗，机械，类人机器等的相关内容"
+                        },
+                        "consumer_electronics": {
+                            "type": "boolean",
+                            "description": "文章是否含有消费电子，智能手机，智能手表等内容"
+                        },
+                    },
+                    "required": ["summary",
+                                 "aigc", "digital_human", "neural_rendering", "computer_graphics", "computer_vision",
+                                 "robotics", "consumer_electronics"],
+                },
+            }
+        }
+    ]
     functions = [{
         "name": function_name,
         "description": "记录文章的总结和标签",
@@ -93,13 +143,14 @@ consumer_electronics: 消费电子，智能手机，智能手表等内容
                          "robotics", "consumer_electronics"],
         },
     }]
-    response = openai.ChatCompletion.create(
-        engine=chatgpt_deployment_name,
+    response = client.chat.completions.create(
+        model=chatgpt_deployment_name,
         messages=messages,
-        functions=functions,
-        function_call={"name": function_name},  # force the model to call the function
+        tools=tools,
+        tool_choice={"type": "function", "function": {"name": function_name}}
     )
-    function_call_info = response['choices'][0]['message']["function_call"]
+
+    function_call_info = response['choices'][0]['message']["tool_calls"][0]["function"]
     assert function_call_info["name"] == function_name
     function_args = json.loads(function_call_info["arguments"])
     feed_item.summary = function_args["summary"]
